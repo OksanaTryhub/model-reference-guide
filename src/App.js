60986ac98729327dc25e1d7dbd28b975337c6f4a
OksanaTryhub <108359208+OksanaTryhub@users.noreply.github.com>
2023-06-05 16:52:29 +0300

    added data-test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import ModelList from './components/ModelList/ModelList';
 
 function App() {
   return (
     <div className="App">
       <h1>Model Reference Guide</h1>
+      <ModelList/>
     </div>
   );
 }
---
+++ src/components/ModelList/ModelList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ModelList = () => {
+  return (
+    <div>ModelList</div>
+  )
+}
+
+export default ModelList
